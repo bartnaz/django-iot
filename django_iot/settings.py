bcc97240b5672dca6ac625f873a638873c5bf2ad
bartnaz <bartosz.nazieblo@stxnext.pl>
2021-10-18 23:44:52 +0200

    feat(user-login) user registration initial commit

--- django_iot/settings.py
+++ django_iot/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = "/"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+# this path('home', views.home, name='user-home') will allow to get it from this url http://localhost:8008/user/home
+
 urlpatterns = [
-    path('rejestracja', views.register, name='registration')
+    path('home', views.home, name='user-home'),
+    path('rejestracja', views.register, name='registration'),
+    path('logowanie', views.login, name='login')
 ]
